1cee1fdca69448ec42945362d5ba3fb37a2c7253
bhcmht1011 <143331757+bhcmht1011@users.noreply.github.com>
2023-11-05 22:14:31 +0800

    Update todolist.js

--- todolist.js
+++ todolist.js
@@ -9,8 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes/todolist.js
-
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/bhcmht1011/txsr/main/todolist.js
 [mitm]
 hostname = buy.itunes.apple.com
 *************************************/
