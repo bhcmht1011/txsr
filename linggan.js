7826c7bbada23ec37f5b91f706fe9933e2726a37
bhcmht1011 <143331757+bhcmht1011@users.noreply.github.com>
2023-11-08 17:33:35 +0800

    Update linggan.js

--- linggan.js
+++ linggan.js
@@ -9,7 +9,7 @@
 [rewrite_local]
 
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/linggan.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/bhcmht1011/txsr/main/linggan.js
 
 [mitm] 
 
